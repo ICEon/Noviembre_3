6a54d6b50b820a5219216d49e950f903091ce67a
Eon Matrix <matrix_eon@yahoo.com>
2012-11-03 13:20:39 -0600

    Ok 2

---
+++ appjs/actions.js
@@ -0,0 +1,103 @@
+//alertas del dispositivo
+
+function pgAlert(mess){
+var title=$('title').text();
+var btnName='Aceptar';
+function error(){
+//alert(mess);
+}
+navigator.notification.alert(mess, error, title, btnName);
+}
+$(document).ready(function(){
+document.addEventListener("deviceready",function(){
+
+//Historial Eventos
+document.addEventListener("pause", function(){//Al pausar la aplicaci�n
+
+var content = ('La aplicaci&oacute;n se paus&oacute; //n');
+eventHistory('La aplicaci&oacute;n se paus&oacute;');
+writeFiles();
+}, false);
+document.addEventListener("resume", function(){//Al volver a la aplicaci�n
+var content = ('La aplicaci&oacute;n ha reaunudado; //n');
+eventHistory('La aplicaci&oacute;n ha reanudado;');
+writeFiles();
+
+navigator.notification.confirm('¿que desea hacer?', function(boton){ 
+			switch(boton){
+				case 1:
+					function readFiles()
+ {
+  window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem)
+   {
+    fileSystem.root.getFile('log.txt', null, function(archivo)
+	 {
+      archivo.file(function(archivo)
+	   {
+        var lector = new FileReader();
+        lector.onloadend = function(e)
+		 {
+          alert(' Version:' + device.version + '//n' + e.target.result);
+         }
+        lector.readAsDataURL(file);
+       }
+	   , function()
+	      {
+           pgAlert("No existe el archivo, agrega contenido y luego presiona en Escribir");
+          }
+	);},
+function(err){
+pgAlert("No se pudo acceder al sistema de archivos");
+});
+},
+function(err){
+pgAlert("No se pudo acceder al sistema de archivos");
+});
+}
+					
+					
+					break;
+				case 2:
+					navigator.notification.vibrate(500);
+					break;
+				}
+		},"Práctica 1","Version, Vibrar, Cancelar");
+		}
+
+}, false);
+
+}, false);
+
+//Escritura de archivos
+function writeFiles(){
+
+window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem){
+fileSystem.root.getFile('log.txt', { create: true }, function(archivo){
+archivo.createWriter(function(escritor){
+escritor.onwrite = function(e){
+pgAlert("El archivo fue escrito Correctamente!");
+};
+//***********************
+
+escritor.write(content);
+
+//***********************
+
+}, function(){
+pgAlert("No existe el archivo, agrega contenido y luego presiona en Escribir");
+});
+}, function(err){
+pgAlert("No se pudo acceder al sistema de archivos");
+});
+}, function(err){
+pgAlert("No se pudo acceder al sistema de archivos");
+});
+}
+
+
+
+
+});
+function eventHistory(action){
+$('#eventsHistory').append('<li>'+action+'</li>');
+}
